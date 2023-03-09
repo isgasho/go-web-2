11c4926104399d4226aa864e01c5d1bffd493c14
KU4NG <1214966109@qq.com>
2023-03-09 23:29:52 +0800

    gin 优雅的启动关闭

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+
+	// 配置服务启动参数
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+
+	// 启动服务
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("服务异常：", err.Error())
+		}
+	}()
+
+	// 等待中断信号，实现优雅的关闭
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("服务即将开始关闭...")
+
+	// 等待 5 秒处理，然后停止服务
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Fatal("服务停止异常：", err.Error())
+	}
+	log.Println("服务关闭完成")
 }
